7c620c5ae6e7fa98b1c902be3575d5b70fdd0166
Fridah-kalee <fridahkalee@gmail.com>
2022-04-10 18:00:13 +0300

    Auto-generated commit [ci skip]

--- src/app/flash.ts
+++
@@ -1,6 +0,0 @@
-export class Flash {
-    public showExplanation: boolean;
-    constructor(public id:number, public title:string, public explanation:string){
-        this.showExplanation=false;
-    }
-}
